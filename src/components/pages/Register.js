014388639ed469831587ea805202460165a50a9f
Alex Wang <jianuo.wang@duke.edu>
2021-01-01 11:07:06 -0600

    commit

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,50 +1,54 @@
-import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
-import firebaseConfig from '../../firebase';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { auth } from "../../firebase";
 
 export default function Register() {
-	const [currUser, setCurrUser] = useState(null);
-	const history = useHistory();
+  const [currUser, setCurrUser] = useState(null);
+  const history = useHistory();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		let {disp_name, email, password, password_conf} = e.target.elements;
+  const handleSubmit = e => {
+    e.preventDefault();
+    let { disp_name, email, password, password_conf } = e.target.elements;
 
-		//verify
-		if(password !== password_conf) {
-			console.log("passwords dont match")
-			return
-		}
+    //verify
+    if (password !== password_conf) {
+      console.log("passwords dont match");
+      return;
+    }
 
-		try {
-			firebaseConfig.auth().createUserWithEmailAndPassword(email.value, password.value);
-			setCurrUser(true);
-		} catch (err) {
-			console.log(err.message);
-		}
-		if (currUser) {
-			history.push('/');
-		}
-	}
+    try {
+      auth.createUserWithEmailAndPassword(email.value, password.value);
+      setCurrUser(true);
+    } catch (err) {
+      console.log(err.message);
+    }
+    if (currUser) {
+      history.push("/");
+    }
+  };
 
-	return (
-		<>
-			<h2>Register</h2>
-			<form onSubmit = {handleSubmit}>
-				<label htmlFor = 'reg-name'>Name</label>
-				<input id = 'reg-name' type = "text" placeholder = "Display Name"/>
+  return (
+    <>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="reg-name">Name</label>
+        <input id="reg-name" type="text" placeholder="Display Name" />
 
-				<label htmlFor = 'reg-email'>Email</label>
-				<input id = 'reg-email' type = "email" placeholder = "email"/>
+        <label htmlFor="reg-email">Email</label>
+        <input id="reg-email" type="email" placeholder="email" />
 
-				<label htmlFor = 'reg-pass'>Password</label>
-				<input id = 'reg-pass-conf' type = "password" placeholder = "Password"/>
+        <label htmlFor="reg-pass">Password</label>
+        <input id="reg-pass-conf" type="password" placeholder="Password" />
 
-				<label htmlFor = 'reg-pass_conf'>Password Confirmation</label>
-				<input id = 'reg-pass-conf' type = "password" placeholder = "Confirm Password"/>
-			
-				<button type = 'submit'>Register</button>
-			</form>
-		</>
-	)
+        <label htmlFor="reg-pass_conf">Password Confirmation</label>
+        <input
+          id="reg-pass-conf"
+          type="password"
+          placeholder="Confirm Password"
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -15,5 +15,3 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
-export default firebaseConfig

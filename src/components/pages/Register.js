7d99b628431c9125947319ce62aa5807d4cbd30f
Alex Wang <jianuo.wang@duke.edu>
2021-01-01 11:38:36 -0600

    register and login

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import firebaseApp from "../../firebase";
@@ -7,20 +6,20 @@ export default function Register() {
   const [currUser, setCurrUser] = useState(null);
   const history = useHistory();
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_conf, setPassword_conf] = useState("");
+
   const handleSubmit = e => {
     e.preventDefault();
-    let { disp_name, email, password, password_conf } = e.target.elements;
-
     //verify
     if (password !== password_conf) {
       console.log("passwords dont match");
       return;
     }
-
     try {
-      firebaseApp
-        .auth()
-        .createUserWithEmailAndPassword(email.value, password.value);
+      firebaseApp.auth().createUserWithEmailAndPassword(email, password);
       setCurrUser(true);
     } catch (err) {
       console.log(err.message);
@@ -35,76 +34,39 @@ export default function Register() {
       <h2>Register</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="reg-name">Name</label>
-        <input id="reg-name" type="text" placeholder="Display Name" />
+        <input
+          id="reg-name"
+          type="text"
+          placeholder="Display Name"
+          onChange={e => setName(e.target.value)}
+        />
 
         <label htmlFor="reg-email">Email</label>
-        <input id="reg-email" type="email" placeholder="email" />
+        <input
+          id="reg-email"
+          type="email"
+          placeholder="email"
+          onChange={e => setEmail(e.target.value)}
+        />
 
         <label htmlFor="reg-pass">Password</label>
-        <input id="reg-pass-conf" type="password" placeholder="Password" />
+        <input
+          id="reg-pass-conf"
+          type="password"
+          placeholder="Password"
+          onChange={e => setPassword(e.target.value)}
+        />
 
         <label htmlFor="reg-pass_conf">Password Confirmation</label>
         <input
           id="reg-pass-conf"
           type="password"
           placeholder="Confirm Password"
+          onChange={e => setPassword_conf(e.target.value)}
         />
 
         <button type="submit">Register</button>
       </form>
     </>
   );
-=======
-import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
-import firebaseApp from '../../firebase';
-
-export default function Register() {
-	const [currUser, setCurrUser] = useState(null);
-	const history = useHistory();
-
-	const [name, setName] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [password_conf, setPassword_conf] = useState('')
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		//verify
-		if(password !== password_conf) {
-			console.log("passwords dont match")
-			return
-		}
-		try {
-			firebaseApp.auth().createUserWithEmailAndPassword(email, password);
-			setCurrUser(true);
-		} catch (err) {
-			console.log(err.message);
-		}
-		if (currUser) {
-			history.push('/');
-		}
-	}
-
-	return (
-		<>
-			<h2>Register</h2>
-			<form onSubmit = {handleSubmit}>
-				<label htmlFor = 'reg-name'>Name</label>
-				<input id = 'reg-name' type = "text" placeholder = "Display Name" onChange = {(e) => setName(e.target.value)}/>
-
-				<label htmlFor = 'reg-email'>Email</label>
-				<input id = 'reg-email' type = "email" placeholder = "email" onChange = {(e) => setEmail(e.target.value)}/>
-
-				<label htmlFor = 'reg-pass'>Password</label>
-				<input id = 'reg-pass-conf' type = "password" placeholder = "Password" onChange = {(e) => setPassword(e.target.value)}/>
-
-				<label htmlFor = 'reg-pass_conf'>Password Confirmation</label>
-				<input id = 'reg-pass-conf' type = "password" placeholder = "Confirm Password" onChange = {(e) => setPassword_conf(e.target.value)}/>
-			
-				<button type = 'submit'>Register</button>
-			</form>
-		</>
-	)
->>>>>>> 93d2c113cdef991c8b59810f5a959a76d9daf269
 }

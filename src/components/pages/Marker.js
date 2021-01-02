957a19531608fe80bfbc5b5072f8aba89e9ba707
Lisa Tang <lt188@duke.edu>
2021-01-02 13:03:31 -0800

    marker page

---
+++ src/components/pages/Marker.js
@@ -0,0 +1,15 @@
+import React from "react";
+import firebase, { db } from "../../firebase";
+import { useHistory } from "react-router-dom";
+import GoogleMapReact from "google-map-react";
+
+export default function Marker() {
+      
+    return (
+      <div>
+        <h1>Marker</h1>
+      </div>
+    );
+  }
+  
+

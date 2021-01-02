06b5f72993f5b7cf41a5881d06fafd8baed49177
Jeff <jeffrey.cheng132@gmail.com>
2021-01-02 15:08:30 -0500

    chat

--- src/components/layout/Chatbox.js
+++ src/components/layout/Chatbox.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import firebase, { auth, db } from "../../firebase";
+import { db } from "../../firebase";
 
 export default function Chatbox({chat}) {
 	const {id, chatName} = chat
 	const [messages, setMessages] = useState([])
-
+	const [text, setText] = useState("");
 	
+	const sendMessage = (e) => {
+		e.preventDefault()
+		if (text !== '') {
+			//push to firebase
+			console.log(text);
+
+			setText('');
+		}
+		
+	}
+
 	useEffect(() => {
 		const unsubscribe = db.collection("chat-messages").doc(id).collection("messages").onSnapshot((snapshot) => {
 			let data = snapshot.docs.map(doc => doc.data()).sort((m1,m2) => {return m1.sentAt - m2.sentAt});
@@ -29,6 +40,10 @@ export default function Chatbox({chat}) {
 				<li>{`active chat is ${chatName}`}</li>
 				{messageList}
 			</ul>
+			<form onSubmit = {sendMessage}>
+				<input type = 'text' value = {text} onChange = {(e) => setText(e.target.value)} placeholder = "send a message"/>
+				<button type = "submit">Send</button>
+			</form>
 		</div>
 	)
 }

4b2706fef6549de9c7d1a7c96d9ce35ce7f43aa0
Alex Wang <jianuo.wang@duke.edu>
2021-01-02 15:07:07 -0600

    map user maker

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import firebase, { db } from "../../firebase";
 import { useHistory } from "react-router-dom";
 import GoogleMapReact from "google-map-react";
@@ -14,21 +14,51 @@ const Map = () => {
     zoom: 11
   };
 
+  const [userLocation, setUserLocation] = useState({ lat: 0, lng: 0 });
+  const [loading, setLoading] = useState("true");
+
+  useEffect(() => {
+    if (navigator.geolocation) console.log("it exists");
+
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        setUserLocation({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        });
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
+
+        setLoading(false);
+      },
+      err => {
+        console.log(err.message);
+      },
+      { timeout: 10000, enableHighAccuracy: true, maximumAge: 75000 }
+    );
+  }, []);
+
   const history = useHistory();
 
   if (firebase.auth().currentUser === null) {
     history.push("/");
   }
 
+  if (loading) return <div>Loading...</div>;
+
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyAJHUXHm2q7jMOk-ecp9itzQS_wrtKKuac" }}
-        defaultCenter={defaultProps.center}
+        defaultCenter={userLocation}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        <AnyReactComponent
+          lat={userLocation.lat}
+          lng={userLocation.lng}
+          text="My Marker"
+        />
       </GoogleMapReact>
     </div>
   );

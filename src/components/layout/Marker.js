c5e5eca6b6a7038a3c0f41bb38cc7f6d1be265cc
kcz3 <kcz3@duke.edu>
2021-01-03 02:00:31 -0500

    marker color change, ineffiecent

--- src/components/layout/Marker.js
+++ src/components/layout/Marker.js
@@ -2,13 +2,33 @@ import React from "react";
 import defaultIco from "../assets/logo.png";
 
 const Marker = (props) => {
-	const { color, name, id, url } = props;
+  const { color, name, id, url } = props;
+  console.log(color);
+  let displayColor = color;
+  switch(color){
+    case "eager":
+      displayColor = "yellow";
+      break;
+    case "available":
+      displayColor = "green";
+      break;
+    case "unavailable":
+      displayColor = "red";
+      break;
+    case "anonymous":
+      displayColor = "gray";
+      break;
+    case "dnd":
+      displayColor = "purple";
+      break;
+  }
+  
   return (
     <div>
       <div
         className="pin bounce"
         style={{
-          backgroundColor: color,
+          backgroundColor: displayColor,
           cursor: "pointer",
         }}
         title={name}

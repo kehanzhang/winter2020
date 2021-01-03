66b5912a9626125f459adfc2a221e2318930adfa
Alex Wang <jianuo.wang@duke.edu>
2021-01-02 20:33:11 -0600

    getting ready to merge

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,23 +1,23 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+// import React, { useContext, useEffect, useRef, useState } from "react";
 
-import { AuthContext } from "../Auth";
-import { useHistory } from "react-router-dom";
+// import { AuthContext } from "../Auth";
+// import { useHistory } from "react-router-dom";
 
-// import MainContainer from "../layout/MainContainer";
-import Logout from "../layout/Logout";
+// // import MainContainer from "../layout/MainContainer";
+// import Logout from "../layout/Logout";
 
-export default function Dashboard() {
-  const { currUser } = useContext(AuthContext);
-  const history = useHistory();
+// export default function Dashboard() {
+//   const { currUser } = useContext(AuthContext);
+//   const history = useHistory();
 
-  if (currUser === null) history.push("/");
+//   if (currUser === null) history.push("/");
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <h2>Hello {currUser === null ? "hi" : currUser.email}</h2>
-      <MainContainer />
-      <Logout />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Dashboard</h1>
+//       <h2>Hello {currUser === null ? "hi" : currUser.email}</h2>
+//       <MainContainer />
+//       <Logout />
+//     </div>
+//   );
+// }
